c8072a868aef198c8d41371ffe4e8f7f136b4032
Peter Boerth <pmboerth@gmail.com>
2024-09-17 01:35:50 -0400

    added contact me component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import ContactMe from "@/components/ContactMe";
 import Education from "@/components/Education";
 import Intro from "@/components/Intro";
 import Skills from "@/components/Skills";
@@ -8,11 +9,13 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="relative w-8/12 mx-auto items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start"></main>
-      <Intro />
-      <WorkExperience />
-      <Education />
-      <Skills />
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <Intro />
+        <WorkExperience />
+        <Education />
+        <Skills />
+        <ContactMe />
+      </main>
     </div>
   );
 }
---
+++ components/ContactMe.tsx
@@ -0,0 +1,15 @@
+
+
+const ContactMe = () => {
+    return (
+        <div className="flex flex-col gap-4">
+            <h1 className="text-2xl font-bold">Contact Me</h1>
+            <p className="text-base text-gray-700">
+                I would love to chat about potential software engineering opportunities or projects! Feel free to shoot me an email directly at&nbsp;
+                <a className="text-blue-500" href="mailto:boerth.p@northeastern.edu">boerth.p@northeastern.edu</a>
+            </p>
+        </div>
+    )
+}
+
+export default ContactMe;
